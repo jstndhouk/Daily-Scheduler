d0e05f8541c44ab8fddaf98c6b15740c788649c7
Justin Houk <jstndhouk@gmail.com>
2021-09-29 18:05:15 -0400

    Made it look a bit more like the mock up.  Added comments to script.js, and added a link to the deployed page to the readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Daily-Scheduler
+# Daily Scheduler
+
+
+## Description
+This application creates a daily work-hours scheduler. The framework was created using bootstraps "text-input" component.  This includes a text on the left (used to indicate the hour) and then an input on the right.  It also includes a jumbotron at the top.  The jumbotron includes the date and time that updates each second.  Each section is colored, indicating whether that hour has passed, or is in the future.  I did this by looping through each text box (brought in with an array with .querySelectorAll) then matching each index of that array with the hour of the time of day.  I then stored each response in local storage when the save button is pressed.  This application could be used to plan out your day, while saving the content of the scheduler even after you close the tab or browser.
+
+The deployed application can be found here: https://jstndhouk.github.io/Daily-Scheduler/
+
+## Screenshots
+![Image output](./assets/images/Capture1.PNG)
+![Image output](./assets/images/Capture2.PNG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,29 @@
+//Gets the date and time at the top of the scheduler
 let timeDate=$('.dtstyle');
 let today=moment().format('MMMM Do YYYY, h:mm:ss a');
 timeDate.text(today);
+//makes it so the date and time refreshes every second
 
 timeInterval=setInterval(function()
 {
-    let today=moment().format('MMMM Do YYYY, h:mm:ss a');
-    timeDate.text(today);
+  let today=moment().format('MMMM Do YYYY, h:mm:ss a');
+  timeDate.text(today);
 },1000)
 
-  let hours=moment().format('H')
-  let tickEl=document.querySelectorAll('.tick');
+//selects the hour of the date
+let hours=moment().format('H')
 
-  for (i=0; i<tickEl.length; i++){
+//selects all of the text boxes
+let tickEl=document.querySelectorAll('.tick');
+
+//loops through the textboxes and puts what is in them into local storage, each with a unique key.
+for (i=0; i<tickEl.length; i++){
   let grabString="event"+i;
-  tickEl[i].value=window.localStorage.getItem(grabString)}
+  tickEl[i].value=window.localStorage.getItem(grabString)
+}
 
-  for(i=0;i<tickEl.length; i++){
+//Loops through the textboxes.  Changes their background color based off their index(associated by a difference of 6 with the hour of the day)
+for(i=0;i<tickEl.length; i++){
   if (i<(hours-6)){
     tickEl[i].setAttribute('style', "background-color: darkgrey")
     }
@@ -23,9 +31,10 @@ timeInterval=setInterval(function()
     tickEl[i].setAttribute('style', "background-color: red")}
   else {
     tickEl[i].setAttribute('style', 'background-color: green')}
-  }
+}
 
-  let submitbtnEl=$('.submitbtn');
+//grabs the submit button and stores the values in the textboxes into local storage to be used in loop in line 20.
+let submitbtnEl=$('.submitbtn');
   submitbtnEl.on('click', function(){
     for (i=0; i<=tickEl.length-1; i++){
       let storageString="event"+i
